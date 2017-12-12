d8a4bc240e8d4dd58c1cd752d2be594cfc5b1bd8
Lavanya Ammi Chandrashekara <lavanya.ammic@gmail.com>
2017-12-12 11:21:46 -0800

    Updated with removevertex and removeEdge functionality

--- graph/graph.js
+++ graph/graph.js
@@ -7,19 +7,42 @@ function Graph (){
 Graph.prototype.addVertex = function(vertex){
 	this.vertices.push(vertex);
 	this.edges[vertex] = [];
-}
+};
+
+Graph.prototype.removeVertex = function(vertex){
+	const index = this.vertices.indexOf(vertex);
+	if(index !== -1){
+		this.vertices.splice(index, 1);
+	}
+	while(this.edges[vertex].length > 0){
+		const adjacentVertex = this.edges[vertex].pop();
+		this.removeEdge(adjacentVertex, vertex);
+	}
+};
 
 Graph.prototype.addEdge = function(vertex1, vertex2){
 	this.edges[vertex1].push(vertex2);
 	this.edges[vertex2].push(vertex1);
 	this.numberOfEdges++;
-}
+};
+
+Graph.prototype.removeEdge = function(vertex1, vertex2){
+	const index1 = this.edges[vertex1]? this.edges[vertex1].indexOf(vertex2): -1;
+	const index2 = this.edges[vertex2]? this.edges[vertex2].indexOf(vertex1): -1;
+	if(index1 !== -1){
+		this.edges[vertex1].splice(index1, 1);
+		this.numberOfEdges--;
+	}
+	if(index2 !== -1){
+		this.edges[vertex2].splice(index2, 1);
+	}
+};
 
 Graph.prototype.print = function(){
 	console.log(this.vertices.map(function(vertex){
 		return (vertex + '->' + this.edges[vertex].join(', ')).trim();
 	}, this).join(' | '));
-}
+};
 
 Graph.prototype.traverseBFS = function(vertex){
 	if(this.vertices.indexOf(vertex) === -1){
@@ -43,7 +66,7 @@ Graph.prototype.traverseBFS = function(vertex){
 			}
 		}
 	}
-}
+};
 
 Graph.prototype.traverseDFS = function(vertex){
 	if(this.vertices.indexOf(vertex) === -1){
@@ -51,7 +74,7 @@ Graph.prototype.traverseDFS = function(vertex){
 	}
 	const visited = [];
 	this._DFShelper(vertex, visited);
-}
+};
 
 Graph.prototype._DFShelper = function(vertex, visited){
 	visited[vertex] = true;
@@ -62,8 +85,14 @@ Graph.prototype._DFShelper = function(vertex, visited){
 			this._DFShelper(this.edges[vertex][i], visited);
 		}
 	}
-}
+};
 
+Graph.prototype.size = function() {
+  return this.vertices.length;
+};
+Graph.prototype.relations = function() {
+  return this.numberOfEdges;
+};
 var graph = new Graph();
 graph.addVertex(1);
 graph.addVertex(2);
@@ -78,7 +107,6 @@ graph.addEdge(2, 5);
 graph.addEdge(3, 4);
 graph.addEdge(4, 5);
 graph.addEdge(4, 6);
-
 graph.print();
 
 graph.traverseBFS(1);
@@ -86,4 +114,13 @@ graph.traverseBFS(1);
 graph.print();
 
 graph.traverseDFS(1);
+
+graph.removeEdge(1, 2);
+graph.print();
+
+graph.removeVertex(5);
+graph.print();
+
+
+
 
