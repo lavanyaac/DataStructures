5a974b7f42bbe2acaaf47a86dc86de232959eaab
Lavanya Ammi Chandrashekara <lavanya.ammic@gmail.com>
2018-01-05 18:09:22 -0800

    toy problems - boggle

--- 20_algos/04_OtherSources/boggle.js
+++ 20_algos/04_OtherSources/boggle.js
@@ -12,4 +12,55 @@
 //          QUIZ
 
 
-	
+function findWord(word){
+	if(!word){
+		return false;
+	}
+	for(const eachword of dictionary){
+		if(eachword === word){
+			return true;
+		}
+	}
+	return false;
+}
+
+function boggleUtil(boggleBoard, visited, i, j, str, output){
+	visited[i][j] = true;
+	str = str+boggleBoard[i][j];
+
+	if(findWord(str)){ output.push(str);}
+
+	for(let row = i-1; row<=i+1 && row<boggleBoard.length; row++){
+		for(let col = j-1; col<=j+1 && col<boggleBoard[0].length; col++){
+			if(row >= 0 && col >= 0 && !visited[row][col]){
+				boggleUtil(boggleBoard, visited, row, col, str, output);
+			}
+		}
+	}
+	visited[i][j] = false;
+	str = str.slice(0, -1);
+}
+
+function findWords(boggleBoard){
+	const visited = [];
+	const output = [];
+
+	for(let i = 0; i<boggleBoard.length; i++){
+		const row = new Array(boggleBoard[0].length).fill(false);
+		visited.push(row);
+	}
+
+	for(let i = 0; i<boggleBoard.length; i++){
+		for(let j = 0; j<boggleBoard[0].length; j++){
+			boggleUtil(boggleBoard, visited, i, j, '', output);
+		}
+	}
+	console.log(output);
+}
+
+const dictionary = ["GEEKS", "FOR", "QUIZ", "GO"];
+const boggle   = [['G','I','Z'],
+               ['U','E','K'],
+               ['Q','S','E']];
+
+findWords(boggle);//[ 'GEEKS', 'QUIZ' ]
