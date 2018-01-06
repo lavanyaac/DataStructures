c63210c80315787c770c62b180f83814f0e12667
Lavanya Ammi Chandrashekara <lavanya.ammic@gmail.com>
2018-01-05 20:25:19 -0800

    toy problems - multiply strings

---
+++ 20_algos/03_LeetCode/multiplyStrings.js
@@ -0,0 +1,35 @@
+// Given two non-negative integers num1 and num2 represented as strings, return the product of num1 and num2.
+
+// Note:
+
+// The length of both num1 and num2 is < 110.
+// Both num1 and num2 contains only digits 0-9.
+// Both num1 and num2 does not contain any leading zero.
+// You must not use any built-in BigInteger library or convert the inputs to integer directly.
+
+/**
+ * @param {string} num1
+ * @param {string} num2
+ * @return {string}
+ */
+const multiply = function(num1, num2) {
+	
+	const m = num1.length;
+	const n = num2.length;
+	const product = new Array(m+n).fill(0);
+
+	for(let i = m-1; i>=0; i--){
+		for(let j = n-1; j>=0; j--){
+			const mul = (parseInt(num1[i])) * (parseInt(num2[j]));
+			const p1 = i+j;
+			const p2 = i+j+1;
+			const sum = product[p2] + mul;
+			product[p2] = sum%10;
+			product[p1] += Math.floor(sum/10);
+		}
+	}
+	return product.join('');
+};
+
+console.log(multiply('232', '567'));//131544
+
