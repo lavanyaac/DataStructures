93d96581fc4324e40f0ee5c262ca52a6ad8228f9
Lavanya Ammi Chandrashekara <lavanya.ammic@gmail.com>
2018-01-07 21:25:18 -0800

    toy problems - prime factors

---
+++ 20_algos/03_LeetCode/primeFactors.js
@@ -0,0 +1,41 @@
+// Given an array of integers, output an array that contains the number of prime factor for each integer. 
+//My solution gets too slow, but maybe it's just python is too slow for this problem.  
+
+const memo = {};
+
+const primeFactors = function(num){
+	const factors = [];
+
+	while(num%2 === 0){
+		num = num/2;
+		factors.push(2);
+	}
+
+	for(let i = 3; i<Math.ceil(Math.sqrt(num)); i++){
+		while(num%i === 0){
+			num = num/i;
+			factors.push(i);
+		}
+	}
+	if(num>2){
+		factors.push(num);
+	}
+
+	return factors;
+}
+
+const primeFactorsArr = function(arr){
+	const out = [];
+
+	for(n of arr){
+		const res = primeFactors(n);
+		out.push(res.length);
+	}
+
+	return out;
+}
+
+
+// console.log(primeFactors(315));
+
+console.log(primeFactorsArr([1,3,4,5,10,12]))
